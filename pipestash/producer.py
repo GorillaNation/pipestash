942bbbf04fad9f3fbf3cab0dd330f214f64ccae1
Jeremy Kitchen <jeremy.kitchen@gorillanation.com>
2013-02-27 15:15:00 -0800

    signal and exception handling in the producer

--- pipestash/producer.py
+++ pipestash/producer.py
@@ -2,30 +2,44 @@
 # also restart the consumer if it dies for some reason
 import sys
 import datetime
+import signal
 
 
 def produce(config, queue, create_consumer):
     consumer = create_consumer()
 
-    while True:
-        # read the line
-        line = sys.stdin.readline()
-        if not line:
-            # EOF
-            break
-        # strip trailing newline
-        line = line.rstrip()
+    try:
+        def graceful(signum, frame):
+            raise Exception("caught {0} signal, exiting".format(signum))
 
-        if config.stdout:
-            print line
+        signal.signal(signal.SIGTERM, graceful)
+        signal.signal(signal.SIGINT, graceful)
+        signal.signal(signal.SIGQUIT, graceful)
 
-        # check the consumer and restart if necessary
-        if not consumer.is_alive():
-            consumer.join()
-            consumer = create_consumer()
+        while True:
+            # read the line
+            line = sys.stdin.readline()
+            if not line:
+                # EOF
+                break
+            # strip trailing newline
+            line = line.rstrip()
 
-        # toss it in the queue
-        queue.put([datetime.datetime.utcnow().isoformat('T') + 'Z', line])
+            if config.stdout:
+                print line
+
+            # check the consumer and restart if necessary
+            if not consumer.is_alive():
+                consumer.join()
+                consumer = create_consumer()
+
+            # toss it in the queue
+            queue.put([datetime.datetime.utcnow().isoformat('T') + 'Z', line])
+    except Exception as e:
+        print "caught exception {0}, queue length ~{1}".format(e, queue.qsize())
+        # FIXME: handle this properly. maybe by putting a message in the queue
+        # if there's an uncaught exception besides an interrupt?
+        pass
     queue.put(None)
     queue.close()
     return consumer
